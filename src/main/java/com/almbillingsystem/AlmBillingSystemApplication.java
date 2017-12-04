3dafe8d48de7c647945dc5e5a62f0eea7e8deb76
rajnish <rajnish08@yahoo.com>
2017-12-04 22:44:40 +0530

    Default changes

---
+++ src/main/java/com/almbillingsystem/AlmBillingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.almbillingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmBillingSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/almbillingsystem/AlmBillingSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.almbillingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlmBillingSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
